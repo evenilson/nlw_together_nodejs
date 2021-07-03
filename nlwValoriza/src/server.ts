01cbeeb381faca1326184467ffeb3df237f3fd16
evenilsonlp <evenilsonlp@gmail.com>
2021-07-03 19:46:39 -0300

    Criando primeira rota

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,6 +1,9 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
+app.get('/test', (request, response) => {
+  return response.send("ola nlw");
+})
 
 app.listen(3001, () => console.log('Server is running'));
