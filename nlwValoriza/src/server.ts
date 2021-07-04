1f4b15b0ccaf5577d3d3cb64d3d2a6477e924345
evenilsonlp <evenilsonlp@gmail.com>
2021-07-04 15:01:31 -0300

    Criando rota post para User

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,8 +1,14 @@
 import "reflect-metadata";
 import express from "express";
 
-import "./database"
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3001, () => console.log('Server is running'));
