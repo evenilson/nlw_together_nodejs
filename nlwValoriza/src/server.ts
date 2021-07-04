fcd0cb65c66d721dd59109049576362174a61863
evenilsonlp <evenilsonlp@gmail.com>
2021-07-04 12:32:08 -0300

    Configurando as micrations

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,13 +1,8 @@
-import express, { request, response } from "express";
+import "reflect-metadata";
+import express from "express";
+
+import "./database"
 
 const app = express();
-
-app.get('/test', (request, response) => {
-  return response.send("ola nlw");
-})
-
-app.post('/test-post', (request, response) => {
-  return response.send("ola nlw post");
-})
 
 app.listen(3001, () => console.log('Server is running'));
