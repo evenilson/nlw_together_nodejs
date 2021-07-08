09349d139d7fd392c76ba658aeb600245e175ab5
evenilsonlp <evenilsonlp@gmail.com>
2021-07-08 19:58:04 -0300

    Criando middlewares para verificar de o usuário é Admin

--- nlwValoriza/src/routes.ts
+++ nlwValoriza/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 
 const router = Router();
@@ -9,7 +10,7 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
 
-router.post("/users", createUserController.handle);
-router.post("/tags", createTagController.handle);
+router.post("/users",createUserController.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router };
