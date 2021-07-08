81630aa72ec9b57e20ab36f5394f4d31f633f7a1
evenilsonlp <evenilsonlp@gmail.com>
2021-07-08 16:49:24 -0300

    Criando o tratamento de exceções

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import "express-async-errors"
 
 import { router } from "./routes";
 
@@ -10,5 +12,19 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) { 
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+
+})
 
 app.listen(3001, () => console.log('Server is running'));
